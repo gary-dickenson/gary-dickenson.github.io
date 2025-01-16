60a7e80c8bb932225c989bcd7582236df197f414
gary-dickenson <gary-dickenson@users.noreply.github.com>
2025-01-16 20:55:31 +0000

    deploy: c268f72c646015edec303e703f28d2144e0670c0

--- assets/About-BHKQHYU3.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/ComingSoon-Ck2eQelk.js","assets/index-Dh86H0WV.js","assets/index-DvhXL4o-.css","assets/ComingSoon-Bb3LUfTj.css"])))=>i.map(i=>d[i]);
-import{j as t,R as o,_ as e}from"./index-Dh86H0WV.js";const n=o.lazy(()=>e(()=>import("./ComingSoon-Ck2eQelk.js"),__vite__mapDeps([0,1,2,3])));function a(){return t.jsx(n,{})}export{a as default};
---
+++ assets/ComingSoon-C3rTEgSK.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Iz6O38Wb.js";function i(){return o.jsx("div",{className:"coming-soon-container",children:"Coming soon..."})}export{i as default};
