c268f72c646015edec303e703f28d2144e0670c0
Gary Dickenson <gary@gddevltd.co.uk>
2025-01-16 20:54:48 +0000

    Update CI

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'G-J5PBBJW7CH'
+}
+
+TagManager.initialize(tagManagerArgs)
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
