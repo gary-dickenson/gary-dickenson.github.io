2c49f4aa2db1b60f134b1ddccb11011b4dafc3d6
Gary Dickenson <gary@gddevltd.co.uk>
2025-01-16 11:16:54 +0000

    Update CI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './styles/App.scss'
 import { Layout } from './components/Layout'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import ReactGA from "react-ga4"
+
+ReactGA.initialize("UA-171084476-1");
 
 const Home = React.lazy(() => import('./components/Home'))
 const About = React.lazy(() => import('./components/About'))
